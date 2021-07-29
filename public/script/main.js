9b44fb85044aad770e9aff3c2d18a800057a4f4e
LeonardoOliveira3010 <65436800+LeonardoOliveira3010@users.noreply.github.com>
2021-07-29 09:44:17 -0300

    Ajustando as rotas e finalizando o modal

---
+++ public/script/main.js
@@ -0,0 +1,61 @@
+import Modal from './modal.js'
+
+const modal = Modal ()
+
+// Pegar as informações do do modal
+const modalTittle = document.querySelector('.modal h2')
+const modalDescription = document.querySelector('.modal p')
+const modalButton = document.querySelector('.modal button')
+
+// Criando uma função para abrir o modal
+const handleClick = (event, check = true) =>{
+    event.preventDefault()
+
+    // Estrutura das propriedades do modal
+    const text = check ? 'Marcar como lido' : 'Excluir'
+    
+    const tittle = `${text} esta pergunta`
+
+    const textDescription = check ? 'Tem certeza que deseja marcar como lida' : 'Deseja excluir'
+
+    const description = `${textDescription} esta pergunta?`
+
+    // Alterando as propriedades da modal
+    modalTittle.innerHTML = tittle
+    modalDescription.innerHTML = description
+    modalButton.innerHTML = ` Sim, ${text.toLowerCase()}`
+    check ? modalButton.classList.remove('red') : modalButton.classList.add('red')
+
+/* ================================================================================== */
+
+    // Formulario
+    const roomId = document.querySelector('#room-id')
+    roomId.dataset.id
+
+    const slug = check ? 'check' : 'delete'
+
+    const form = document.querySelector('.modal form')
+    form.setAttribute('action', `/room/${roomId}/:question/${slug}`)
+
+/* ================================================================================== */
+
+    modal.open()
+}
+
+// Criação do algoritmo para deletar um post
+const checkButtons = document.querySelectorAll('.actions a.check')
+
+// Percorrer os botões de check
+checkButtons.forEach(button => {
+    // adicionar o evento
+    button.addEventListener('click', handleClick)
+})
+
+// Criação do algoritmo para deletar um post
+const deleteButtons = document.querySelectorAll('.actions a.delete')
+
+// Percorrer os botões de check
+deleteButtons.forEach(button => {
+    button.addEventListener('click', (event) => handleClick(event, false))
+})
+
--- src/route.js
+++ src/route.js
@@ -2,10 +2,13 @@ const express = require('express')
 
 const route = express.Router()
 
-route.get('/', (req, res) => res.render('index'))
+route.get('/home', (req, res) => res.render('index'))
 
 route.get('/room', (req, res) => res.render('room'))
 
 route.get('/create-pass', (req, res) => res.render('create-pass'))
+
+// Fomato que o formulario do modal deve passar a informação
+
 
 module.exports = route
